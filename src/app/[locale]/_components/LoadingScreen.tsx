bfcbbd240f0a46d90822a1d1d29c48cc88fb7ba8
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-05-24 17:57:21 +0200

    feat: improve initial loading screen

--- src/app/[locale]/_components/LoadingScreen.tsx
+++ src/app/[locale]/_components/LoadingScreen.tsx
@@ -9,7 +9,16 @@ export default function LoadingScreen({
 }) {
   const loading = useLoading();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className="my-auto flex flex-col items-center space-y-4">
+        <span className="inline-flex h-16 w-16 animate-spin-slow rounded-full border-8 border-dotted border-brand-300"></span>
+        <div className="max-w-xs text-center">
+          <h1 className="text-xl font-bold text-white">Loading...</h1>
+          <p className="mt-2 text-gray-400">Please wait.</p>
+        </div>
+      </div>
+    );
 
   return <>{children}</>;
 }
