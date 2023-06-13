a4c7f0fd0bdba00174a57dd83e3e4d75bb364b68
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-06-13 10:55:14 +0200

    feat: migrate onboarding access to i18n

--- src/app/[locale]/onboarding/access/_components/Methods.tsx
+++ src/app/[locale]/onboarding/access/_components/Methods.tsx
@@ -1,28 +1,30 @@
 "use client";
 
-import { CheckCircleIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { type TypeOptions } from "i18next";
 import DetailCard from "~/components/data/DetailCard";
-
-const METHODS = [
-  {
-    name: "Enter your Discord link",
-    href: "/onboarding/access/link",
-  },
-  {
-    name: "Transfer the email",
-    href: "/onboarding/access/email",
-  },
-];
+import { useTranslation } from "~/i18n/client";
 
 export default function Methods() {
+  const { t } = useTranslation();
+
+  const rawData = t("onboarding.access.methods", { returnObjects: true });
+  const methods = (
+    Object.keys(
+      rawData
+    ) as (keyof TypeOptions["resources"]["translation"]["onboarding"]["access"]["methods"])[]
+  ).map((key) => {
+    return { ...rawData[key], key };
+  });
+
   return (
     <div className="grid w-full grid-cols-1 gap-2">
-      {METHODS.map(({ name, href }, i) => (
+      {methods.map(({ key, title, href }, i) => (
         <DetailCard
-          key={href}
+          key={key}
           href={href}
-          title={name}
-          description={`Method ${i + 1}`}
+          title={title}
+          description={t("onboarding.access.method", { n: i + 1 })}
           reverseTexts
           rightIcon={ChevronRightIcon}
         />
--- src/app/[locale]/onboarding/access/page.tsx
+++ src/app/[locale]/onboarding/access/page.tsx
@@ -8,15 +8,16 @@ export default async function Page({ params: { locale } }: PageProps) {
   return (
     <div className="flex flex-col items-center space-y-4">
       <div className="max-w-xs text-center">
-        <h1 className="text-xl font-bold text-white">I want the stats!</h1>
-        <p className="mt-2 text-gray-400">Choose your preferred method.</p>
+        <h1 className="text-xl font-bold text-white">
+          {t("onboarding.access.title")}
+        </h1>
+        <p className="mt-2 text-gray-400">
+          {t("onboarding.access.description")}
+        </p>
       </div>
       <Methods />
       <div className="max-w-xs text-center">
-        <p className="text-gray-400">
-          Keep in mind it can take up to 30 days to receive the email. Be
-          patient!
-        </p>
+        <p className="text-gray-400">{t("onboarding.shared.30DaysDelay")}</p>
       </div>
     </div>
   );
