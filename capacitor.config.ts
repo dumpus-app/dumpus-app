96a85069edab14feec5d5ccb7b6add1783134eee
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-06-19 16:46:07 +0200

    chore: remove todo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { networkInterfaces } from "os";
 
 const isDev = process.env.NODE_ENV === "development" || false;
 const networks = networkInterfaces();
-const networkKey = "Ethernet" in networks ? "Ethernet" : "Wi-Fi"; // TODO: check if first key is enough
+const networkKey = "Ethernet" in networks ? "Ethernet" : "Wi-Fi";
 const network = networks[networkKey]!;
 const ipAddress = network.find((e) => e.family === "IPv4")!.address;
 
