24571748d6e3be90588032fbb023f9ef8882d0e7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-06-20 10:35:07 +0200

    fix: i18n wrong locale

--- src/i18n/client.ts
+++ src/i18n/client.ts
@@ -9,8 +9,7 @@ import resourcesToBackend from "i18next-resources-to-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { getOptions } from "./settings";
 
-//
-i18next
+export const i18nInstance = i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(
