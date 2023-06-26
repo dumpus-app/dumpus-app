bbe2f1573a23985592769fa52146e9849c25c777
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-06-26 17:23:30 +0200

    fix: export all locales

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -1,12 +1,11 @@
 import { _locales } from "./_locales";
 
-export const locales = _locales;
-
 export const fallbackLocale = "en";
+export const locales = [fallbackLocale, ..._locales];
 
 export function getOptions(locale = fallbackLocale) {
   return {
-    supportedLngs: [fallbackLocale, ...locales],
+    supportedLngs: locales,
     fallbackLng: fallbackLocale,
     lng: locale,
   };
