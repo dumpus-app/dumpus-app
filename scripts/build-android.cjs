2162e8b8a5f38bcd91737ba647ea587bc3af488c
Androz2091 <androz2091@gmail.com>
2023-06-29 19:14:47 +0200

    fix apk sign

--- scripts/build-android.cjs
+++ scripts/build-android.cjs
@@ -16,6 +16,13 @@ console.log("Run pnpm build:mobile first");
 
 const command = `cross-env-shell ${ANDROID_HOME_PATH ? `ANDROID_HOME_PATH="${ANDROID_HOME_PATH}"` : ''} NODE_ENV=production pnpm cap build android --keystorepath ${ANDROID_KEYSTORE_PATH} --keystorepass '${ANDROID_KEYSTORE_PASSWORD}' --keystorealias ${keystoreAlias} --keystorealiaspass '${ANDROID_KEYSTORE_PASSWORD}' --androidreleasetype ${androidReleaseType}`;
 
+const outputPath = `./android/app/build/outputs/apk/release`;
+
+const outputUnsignedFilePath = path.join(outputPath, `app-release-unsigned.apk`);
+const outputApkSignedFilePath = path.join(outputPath, `app-release-apksigner-signed.apk`);
+
+const signCommand = `cp ${outputUnsignedFilePath} ${outputApkSignedFilePath} && apksigner sign --ks ${ANDROID_KEYSTORE_PATH} --ks-key-alias ${keystoreAlias} --ks-pass pass:${ANDROID_KEYSTORE_PASSWORD} --key-pass pass:${ANDROID_KEYSTORE_PASSWORD} ${outputApkSignedFilePath}`;
+
 // Execute the npm script
 const npmProcess = exec(command);
 
@@ -32,4 +39,17 @@ npmProcess.stderr.on("data", (data) => {
 // Listen for the child process to exit
 npmProcess.on("close", (code) => {
   console.log(`child process exited with code ${code}`);
+
+  if (code === 0) {
+    const signProcess = exec(signCommand);
+    signProcess.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+    signProcess.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+    signProcess.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }
 });
