80f7affc6ac04c48aecaad392e19f9110ee79e4d
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-06-29 11:41:21 +0200

    feat: add sentiment score meanings (close #114)

--- src/app/[locale]/(nav)/(time-selector)/(top)/top/dms/details/_components/SentimentScore.tsx
+++ src/app/[locale]/(nav)/(time-selector)/(top)/top/dms/details/_components/SentimentScore.tsx
@@ -38,29 +38,35 @@ export default function SentimentScore({ score }: { score: number }) {
 
   return (
     <Section title="Sentiment analysis (experimental)">
-      <div className="mx-2 grid h-8 grid-cols-9 rounded-full bg-white sm:h-12">
-        {colors.map((color, i) => {
-          const active = i === activeIndex;
-          return (
-            <div
-              key={i}
-              className={clsx(
-                "flex items-center justify-center first:rounded-l-full last:rounded-r-full",
-                color
-              )}
-              aria-label={active ? `Active with score: ${score}` : undefined}
-            >
-              {active && (
-                <div className="relative">
-                  <div className="h-4 w-4 rounded-full bg-gray-900 sm:h-6 sm:w-6"></div>
-                  <div className="absolute -bottom-6 left-1/2 -translate-x-1/2 rounded-full border border-gray-600 bg-gray-800 px-2 text-sm text-gray-300 sm:-bottom-7 sm:text-base">
-                    {score.toFixed(3)}
+      <div className="mx-2 space-y-2">
+        <div className="flex items-center justify-between px-4 text-sm sm:px-6 sm:text-base">
+          <div className="text-danger-500">Anger</div>
+          <div className="text-success-500">Happiness</div>
+        </div>
+        <div className="grid h-8 grid-cols-9 rounded-full bg-white sm:h-12">
+          {colors.map((color, i) => {
+            const active = i === activeIndex;
+            return (
+              <div
+                key={i}
+                className={clsx(
+                  "flex items-center justify-center first:rounded-l-full last:rounded-r-full",
+                  color
+                )}
+                aria-label={active ? `Active with score: ${score}` : undefined}
+              >
+                {active && (
+                  <div className="relative">
+                    <div className="h-4 w-4 rounded-full bg-gray-900 sm:h-6 sm:w-6"></div>
+                    <div className="absolute -bottom-6 left-1/2 -translate-x-1/2 rounded-full border border-gray-600 bg-gray-800 px-2 text-sm text-gray-300 sm:-bottom-7 sm:text-base">
+                      {score.toFixed(3)}
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          );
-        })}
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Section>
   );
