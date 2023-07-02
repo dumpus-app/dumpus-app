64f2e8793c2eda2a0e6c0c777c63714f4122acd0
Androz2091 <androz2091@gmail.com>
2023-07-03 01:30:51 +0200

    add new stats

--- src/app/[locale]/(nav)/(time-selector)/stats/page.tsx
+++ src/app/[locale]/(nav)/(time-selector)/stats/page.tsx
@@ -4,6 +4,11 @@ import PageHeader from "./_components/PageHeader";
 import Stats from "./_components/Stats";
 import SendingTimes from "./_components/SendingTimes";
 import DailySentMessages from "./_components/DailySentMessages";
+import SimplePieChart from "~/components/data/PieChart";
+import Section from "~/components/Section";
+import DetailCard from "~/components/data/DetailCard";
+import { ChartBarIcon } from "@heroicons/react/24/solid";
+import StatCard from "~/components/data/StatCard";
 
 export default function Page() {
   return (
@@ -12,6 +17,17 @@ export default function Page() {
       <Stats />
       <SendingTimes />
       <DailySentMessages />
+{/*       
+      <Section title="">
+        <div className="grid grid-cols-1 gap-2 px-2 py-4 desktop-container sm:grid-cols-2 sm:py-8 md:grid-cols-3">
+          <div className="grid gap-2 px-2">
+            <StatCard value="100" label="app starts" />
+            <StatCard value="100" label="app starts" />
+            <StatCard value="100" label="app starts" />
+          </div>
+          <SimplePieChart data={[{ label: "coucou", value: 90 }, { label: 'Androd', value: 10 }]} legend="iOS" />
+        </div>
+      </Section> */}
     </>
   );
 }
