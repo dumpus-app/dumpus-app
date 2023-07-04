84eecc4d9e90607e660f5e47314fada5df8f2bd6
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-07-04 16:25:56 +0200

    fix: download when share unavailable (close #157)

--- src/app/[locale]/(app)/_components/SharePopup.tsx
+++ src/app/[locale]/(app)/_components/SharePopup.tsx
@@ -4,8 +4,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { useAtom } from "jotai";
 import Button from "~/components/Button";
 import { generatingShareImageAtom, showSharePopupAtom } from "~/stores/ui";
-import { useState, Fragment } from "react";
-import { useMount } from "react-use";
+import { useState, Fragment, useEffect } from "react";
 import useGenerateImg from "~/hooks/use-generate-img";
 import Image from "next/image";
 import useUserData from "~/hooks/data/use-user-data";
@@ -14,8 +13,6 @@ import { formatNumber } from "~/utils/format";
 import useTopDMsData from "~/hooks/data/use-top-dms-data";
 import { avatarURLFallback } from "~/utils/discord";
 import useTopGuildsData from "~/hooks/data/use-top-guilds-data";
-import useToast from "~/hooks/use-toast";
-import { XCircleIcon } from "@heroicons/react/24/solid";
 import { BASE_URL } from "~/constants";
 
 export default function SharePopup() {
@@ -23,7 +20,6 @@ export default function SharePopup() {
   const [generatingShareImage, setGeneratingShareImage] = useAtom(
     generatingShareImageAtom
   );
-  const toast = useToast();
 
   const { init, generate } = useGenerateImg();
   const [url, setUrl] = useState<string>();
@@ -34,45 +30,55 @@ export default function SharePopup() {
   const { getData: getDMsData } = useTopDMsData();
   const { getData: getGuildsData } = useTopGuildsData();
 
-  const genData = {
-    user: {
-      displayName: data.package_owner_display_name,
-      avatarURL: data.package_owner_avatar_url,
-    },
-    stats: {
-      messagesSent: formatNumber(messageCount(), { notation: "standard" }),
-      timeSpent: "N/A",
-      appOpenings: "N/A",
-      otherStat: "N/A",
-    },
-    topDMS: (getDMsData({}) || []).slice(0, 3).map((dm) => {
-      return {
-        name: dm.user_name,
-        // TODO: get latest url
-        url: avatarURLFallback(dm.user_avatar_url, dm.dm_user_id),
-      };
-    }),
-    topGuilds: (getGuildsData({}) || []).slice(0, 3).map((guild) => {
-      return {
-        name: guild.guild_name,
-        url: "https://cdn.discordapp.com/embed/avatars/0.png",
-      };
-    }),
-  };
+  useEffect(() => {
+    if (!open || !generatingShareImage) return;
 
-  async function gen() {
-    const { svgURL, file } = await generate(genData);
-    setUrl(svgURL);
-    setFile(file);
-    setGeneratingShareImage(false);
-  }
+    async function gen() {
+      const { svgURL, file } = await generate({
+        user: {
+          displayName: data.package_owner_display_name,
+          avatarURL: data.package_owner_avatar_url,
+        },
+        stats: {
+          messagesSent: formatNumber(messageCount(), { notation: "standard" }),
+          timeSpent: "N/A",
+          appOpenings: "N/A",
+          otherStat: "N/A",
+        },
+        topDMS: (getDMsData({}) || []).slice(0, 3).map((dm) => {
+          return {
+            name: dm.user_name,
+            // TODO: get latest url
+            url: avatarURLFallback(dm.user_avatar_url, dm.dm_user_id),
+          };
+        }),
+        topGuilds: (getGuildsData({}) || []).slice(0, 3).map((guild) => {
+          return {
+            name: guild.guild_name,
+            url: "https://cdn.discordapp.com/embed/avatars/0.png",
+          };
+        }),
+      });
+      setUrl(svgURL);
+      setFile(file);
+      setGeneratingShareImage(false);
+    }
 
-  useMount(() => {
-    if (!generatingShareImage) return;
     init().then(() => {
       gen();
     });
-  });
+  }, [
+    data.package_owner_avatar_url,
+    data.package_owner_display_name,
+    generate,
+    generatingShareImage,
+    getDMsData,
+    getGuildsData,
+    init,
+    messageCount,
+    open,
+    setGeneratingShareImage,
+  ]);
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -134,6 +140,7 @@ export default function SharePopup() {
                   variant="brand"
                   className="mt-4 w-full"
                   onClick={async () => {
+                    // TODO: detect os as well
                     try {
                       await navigator.share({
                         title: "Here is my Discord recap!",
@@ -142,13 +149,13 @@ export default function SharePopup() {
                         files: [file!],
                       });
                     } catch (err) {
-                      // TODO: download instead
-                      toast({
-                        variant: "danger",
-                        title: "Can't share",
-                        description: "Open an issue on GitHub if you need help",
-                        icon: XCircleIcon,
-                      });
+                      const a = document.createElement("a");
+                      document.body.appendChild(a);
+                      a.setAttribute("style", "display: none");
+                      a.setAttribute("href", url!);
+                      a.download = "dumpus-share.png";
+                      a.click();
+                      a.remove();
                     }
                     setOpen(false);
                   }}
