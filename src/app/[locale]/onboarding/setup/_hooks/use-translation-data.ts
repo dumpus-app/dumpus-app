543ff25013c66e2634dced21285a249652f566dd
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-07-19 09:57:23 +0200

    fix: remove images from locales

---
+++ src/app/[locale]/onboarding/setup/_hooks/use-translation-data.ts
@@ -0,0 +1,55 @@
+"use client";
+
+import { ComputerDesktopIcon } from "@heroicons/react/24/solid";
+import i18next from "i18next";
+import { useMemo } from "react";
+import { SimpleIconsAndroid, SimpleIconsIos } from "~/components/icons";
+import { type OS } from "~/hooks/use-os";
+import { useTranslation } from "~/i18n/client";
+import type { Icon } from "~/types";
+
+const icons: Record<OS, Icon> = {
+  android: SimpleIconsAndroid,
+  ios: SimpleIconsIos,
+  desktop: ComputerDesktopIcon,
+};
+
+type Data = {
+  os: string;
+  display: string;
+  icon: Icon;
+  steps: {
+    name: string;
+    image: string;
+  }[];
+};
+
+export default function useTranslationData() {
+  const { t } = useTranslation();
+
+  const data = useMemo(() => {
+    const osDisplays = t("onboarding.setup.osDisplays", {
+      returnObjects: true,
+    });
+    const steps = t("onboarding.setup.steps", { returnObjects: true });
+    const data: Data[] = [];
+    for (const [os, display] of Object.entries(osDisplays)) {
+      const _data: Data = {
+        os,
+        display,
+        icon: icons[os as OS],
+        steps: [],
+      };
+      for (const [step, name] of Object.entries(steps)) {
+        _data.steps.push({
+          name,
+          image: `/assets/onboarding/${i18next.language}/${os}/${step}.png`,
+        });
+      }
+      data.push(_data);
+    }
+    return data;
+  }, [t]);
+
+  return data;
+}
