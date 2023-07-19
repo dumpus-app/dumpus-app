dd32776aae6fda82ac80a63b6e19ae8df01c1e8d
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-07-19 10:03:39 +0200

    Update use-focus.ts

--- src/hooks/use-focus.ts
+++ src/hooks/use-focus.ts
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function useFocus<E extends HTMLElement>(
   target?: React.RefObject<E> | null,
-  onFocusChange?: (focused: boolean) => void;
+  onFocusChange?: (focused: boolean) => void
 ) {
   const [focused, setFocused] = useState(() => {
     return window.document.activeElement != null;
