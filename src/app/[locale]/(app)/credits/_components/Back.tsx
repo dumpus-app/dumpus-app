7548dc733907df6d6c8832176c9a08d8a3cfd143
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-07-20 10:56:53 +0200

    fix: show back button on sm breakpoint

--- src/app/[locale]/(app)/credits/_components/Back.tsx
+++ src/app/[locale]/(app)/credits/_components/Back.tsx
@@ -5,7 +5,7 @@ import Link from "~/components/Link";
 
 export default function Back() {
   return (
-    <div className="hidden px-4 pt-4 desktop-container md:block">
+    <div className="hidden px-4 pt-4 desktop-container sm:block">
       <Link
         href="/settings"
         className="-ml-2 inline-flex items-center rounded-md p-2 transition-colors hover:bg-gray-800 hover:text-white"
