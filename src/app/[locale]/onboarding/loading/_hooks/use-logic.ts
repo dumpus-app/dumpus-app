45108c19e726ae3482da3915d6b0efbe274296fd
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-07-20 13:31:01 +0200

    Update src/app/[locale]/onboarding/loading/_hooks/use-logic.ts
    Co-authored-by: Archaeopteryx <46562212+skdhg@users.noreply.github.com>

--- src/app/[locale]/onboarding/loading/_hooks/use-logic.ts
+++ src/app/[locale]/onboarding/loading/_hooks/use-logic.ts
@@ -67,7 +67,7 @@ function useStatus({
   });
 
   useEffect(() => {
-    setErroredStatus(data?.isErrored || false);
+    setErroredStatus(!!data?.isErrored);
   }, [data]);
 
   return { data, status };
