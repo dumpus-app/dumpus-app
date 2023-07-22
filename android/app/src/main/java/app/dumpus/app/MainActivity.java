843b68442bd589e0ab4e6a78d3352e5975c98094
Androz2091 <androz2091@gmail.com>
2023-07-22 16:24:49 +0200

    android working

--- android/app/src/main/java/app/dumpus/app/MainActivity.java
+++ android/app/src/main/java/app/dumpus/app/MainActivity.java
@@ -1,21 +1,5 @@
 package app.dumpus.app;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        WebView webview = getBridge().getWebView();
-        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webview.setVerticalScrollBarEnabled(false);
-    }
-}
+public class MainActivity extends BridgeActivity {}
