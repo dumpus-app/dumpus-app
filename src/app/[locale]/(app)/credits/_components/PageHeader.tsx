100e1ac5f654e5d19edde45ba2498869ece0df3d
Mr¤KayJayDee <minitouchilink@gmail.com>
2023-07-22 12:06:11 +0200

    Translation credits (#257)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/app/[locale]/(app)/credits/_components/PageHeader.tsx
+++ src/app/[locale]/(app)/credits/_components/PageHeader.tsx
@@ -2,11 +2,13 @@
 
 import Header from "~/components/layout/Header";
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { useTranslation } from "~/i18n/client";
 
 export default function PageHeader() {
+  const { t } = useTranslation();
   return (
     <Header
-      title="Credits"
+      title={t("credits.title")}
       transparent
       revealBorderOnScroll
       revealBackgroundOnScroll
