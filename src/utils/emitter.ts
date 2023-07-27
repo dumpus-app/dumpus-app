43ce9267c9c78812120d70701ba3a077586a64cc
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-07-27 22:57:16 +0200

    close #283 (#292)

--- src/utils/emitter.ts
+++ src/utils/emitter.ts
@@ -1,11 +1,9 @@
 import mitt from "mitt";
-import { purchases } from "~/capacitor";
-import type { ProductKey } from "~/capacitor/purchases";
+import { ProductKey } from "~/stores/purchases";
 
 export const emitter = mitt<{
   "purchases:initialized": boolean;
   "purchases:transaction:approved": {
     key: ProductKey;
-    product: Awaited<NonNullable<ReturnType<typeof purchases.getProduct>>>;
   };
 }>();
